38da8652b7f847a115235f506a112fe926c4c3d1
Victor Ude <victor@lingolion.me>
2017-11-13 15:12:46 -0600

    adding generated devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
+  devise_for :users, controllers: {
+      sessions: 'users/sessions'
+  }
+
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
